28677f79acec7af05df675d979f1e140825545d2
Jesus Arath <jesus.flores.p19@gmail.com>
2021-11-02 21:05:07 -0600

    Agregamos llaves de proyecto

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyAaiF03WGAZnnhNN2He0pnSr6XN1zIN3i4",
+    authDomain: "firechat-ex1.firebaseapp.com",
+    databaseURL: "https://firechat-ex1-default-rtdb.firebaseio.com",
+    projectId: "firechat-ex1",
+    storageBucket: "firechat-ex1.appspot.com",
+    messagingSenderId: "813785645896",
+    appId: "1:813785645896:web:c53ee91b9fd5700612297d"
+  }
 };
 
 /*
