f957988bc07abe3b6daca5fe7f146fab8454f543
Jesus Arath <jesus.flores.p19@gmail.com>
2021-11-02 23:51:23 -0600

    Algunos cambios del observable

--- src/app/service/firechat.service.ts
+++ src/app/service/firechat.service.ts
@@ -1,6 +1,9 @@
 import { Injectable } from '@angular/core';
 import { Firestore, collectionData, collection} from '@angular/fire/firestore';
-// import {collection} from "firebase/firestore"
+import { Observable } from 'rxjs';
+import {map} from "rxjs/operators";
+import { Mensaje } from "../interface/mensaje.interface"
+
 
 @Injectable({
   providedIn: 'root'
@@ -9,8 +12,16 @@ export class FirechatService {
  
   constructor(private firestore: Firestore) { }
 
+  public chats: Mensaje[]= [];
+
   obtenerCollection(){
-    const dataCol = collection(this.firestore, 'familia');
-    return collectionData(dataCol);
+    const dataCol = collection(this.firestore, 'chats');
+    return collectionData(dataCol)
+                        .pipe( map( (data) => {
+                           const mensajes = data as Mensaje[]
+                           this.chats = mensajes
+                           console.log(this.chats);
+                        }))
+                        
   }
 }
