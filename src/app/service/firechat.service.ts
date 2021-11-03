80cb37c259e22717410ce70bfa11afd67075cab3
Jesus Arath <jesus.flores.p19@gmail.com>
2021-11-02 21:40:22 -0600

    Conexion con servicio firebase

--- src/app/service/firechat.service.ts
+++ src/app/service/firechat.service.ts
@@ -1,9 +1,16 @@
 import { Injectable } from '@angular/core';
+import { Firestore, collectionData, collection} from '@angular/fire/firestore';
+// import {collection} from "firebase/firestore"
 
 @Injectable({
   providedIn: 'root'
 })
 export class FirechatService {
+ 
+  constructor(private firestore: Firestore) { }
 
-  constructor() { }
+  obtenerCollection(){
+    const dataCol = collection(this.firestore, 'familia');
+    return collectionData(dataCol);
+  }
 }
