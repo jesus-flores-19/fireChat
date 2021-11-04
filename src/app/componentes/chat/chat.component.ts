e14410a5c5da9d645da08fba97ee03d0601bdde3
Jesus Arath <jesus.flores.p19@gmail.com>
2021-11-03 22:45:44 -0600

    Agregar mensaje

--- src/app/componentes/chat/chat.component.ts
+++ src/app/componentes/chat/chat.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Mensaje } from 'src/app/interface/mensaje.interface';
 import { FirechatService } from 'src/app/service/firechat.service';
 
 @Component({
@@ -10,17 +11,21 @@ export class ChatComponent implements OnInit {
 
   constructor(public firechat: FirechatService) { 
     this.firechat.obtenerCollection().subscribe();
-    console.log(this.firechat.chats);
-    
   }
 
-  mensaje: any = "";
+  data: Mensaje= {
+    mensaje: ""
+  };
 
   ngOnInit(): void {
   }
 
   enviarMensaje(){
-    console.log(this.mensaje);
+    if(this.data.mensaje.length == 0) return;
+    
+    console.log(this.data);
+    this.firechat.agregarMensaje(this.data)
+    
   }
 
 }
