ce2e91d02cf8b9ecc49e8c72afc47eac3a2f7988
Jesus Arath <jesus.flores.p19@gmail.com>
2021-11-04 21:37:55 -0600

    uso de funciones del servicio

--- src/app/componentes/chat/chat.component.ts
+++ src/app/componentes/chat/chat.component.ts
@@ -10,21 +10,39 @@ import { FirechatService } from 'src/app/service/firechat.service';
 export class ChatComponent implements OnInit {
 
   constructor(public firechat: FirechatService) { 
-    this.firechat.obtenerCollection().subscribe();
+    
   }
 
   data: Mensaje= {
-    mensaje: ""
+    mensaje: "",
+    nombre: ""
   };
+  elemento: any;
 
   ngOnInit(): void {
+    this.elemento = document.getElementById("app-mensajes")
+  }
+
+  usuario(nombre: any){
+    this.data.nombre = nombre
+    this.firechat.obtenerCollection().subscribe( () => {
+      setTimeout(()=>{
+        this.elemento.scrollTop = this.elemento.scrollHeight;
+      },20)
+    });
   }
 
   enviarMensaje(){
     if(this.data.mensaje.length == 0) return;
+    if(this.data.nombre?.length==0){
+      alert("escogueee un usuario xd")
+      return;
+    }
     
     console.log(this.data);
     this.firechat.agregarMensaje(this.data)
+                  .then( ()=> this.data.mensaje ="" )
+                  .catch(()=> console.error("Error"))
     
   }
 
